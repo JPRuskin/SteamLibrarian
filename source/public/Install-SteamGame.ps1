98fe9950be91b37ce22e1d57a6818302a1b2d0cc
James Ruskin <james.ruskin@questionmark.com>
2018-12-15 10:38:23 +0000

    Improve Install-SteamGame Logic
    - Adds ArgumentCompleter as attribute rather than external registration
    - Adds positional parameter value for Name and ID
    - Adds better error handling to the parameter validation

--- source/public/Install-SteamGame.ps1
+++ source/public/Install-SteamGame.ps1
@@ -1,36 +1,48 @@
 function Install-SteamGame {
     [CmdletBinding(DefaultParameterSetName = "Name")]
     param(
-        [Parameter(Mandatory, ParameterSetName = "Name")]
-        [ValidateScript({$_ -in (Get-SteamGameList).name})]
+        [ArgumentCompleter({
+            param($commandName, $parameterName, $wordToComplete)
+            $MatchString = "^$($wordToComplete -replace '^["'']' -replace "[^A-z0-9]",'.*?')"
+            Write-Debug "Finding games that match '$MatchString'"
+
+            $MatchList = [System.Collections.ArrayList]::new()
+            (Get-SteamGameList).name | % {
+                if ($_ -match $MatchString) {$null = $MatchList.Add($_)}
+            }
+
+            Write-Debug "Found $($MatchList.Count) games that match '$($MatchString)'"
+            $MatchList | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new("`"$_`"", $_, 'ParameterValue', $_)
+            }
+        })]
+        [Parameter(Mandatory, ParameterSetName = "Name", Position = 0)]
+        [ValidateScript({
+            if ($_ -in (Get-SteamGameList).name) {
+                $true
+            } else {
+                throw "'$_' is not a valid Steam Application Name."
+            }
+        })]
         [string]$Name,
 
-        [Parameter(Mandatory, ParameterSetName = "ID")]
-        [ValidateScript({$_ -in (Get-SteamGameList).appid})]
+        [Parameter(Mandatory, ParameterSetName = "ID", Position = 0)]
+        [ValidateScript({
+            if ($_ -in (Get-SteamGameList).appid) {
+                $true
+            } else {
+                throw "ID '$_' is not a valid Steam Application ID."
+            }
+        })]
         [Alias("ApplicationId")]
         [int]$ID
     )
     process {
         if ($PSCmdlet.ParameterSetName -eq "Name") {
             $ID = (Get-SteamGameList).Where{$_.Name -eq $Name}.appid
+            Write-Debug "'$($Name)' has ID '$($ID)'"
         }
 
         [SteamGame]::Install($ID)
-    }
-}
-
-Register-ArgumentCompleter -CommandName Install-SteamGame -ParameterName Name -ScriptBlock {
-    param($commandName, $parameterName, $wordToComplete)
-    $MatchString = "^$($wordToComplete -replace '^["'']' -replace "[^A-z0-9]",'.*?')"
-    Write-Debug "Finding games that match '$MatchString'"
-
-    $MatchList = [System.Collections.ArrayList]::new()
-    (Get-SteamGameList).name | % {
-        if ($_ -match $MatchString) {$null = $MatchList.Add($_)}
-    }
-
-    Write-Debug "Found $($MatchList.Count) games that match '$($MatchString)'"
-    $MatchList | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new("`"$_`"", $_, 'ParameterValue', $_)
     }
 }
